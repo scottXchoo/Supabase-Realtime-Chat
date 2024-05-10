5feeea2b110b7890f451469812df5113751a1676
scottXchoo <ckh0601@cau.ac.kr>
2024-05-10 20:06:55 +0900

    feat : add ListMessage component into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/ChatHeader";
 import ChatInput from "@/components/ChatInput";
+import ListMessage from "@/components/ListMessage";
 import InitUser from "@/lib/store/InitUser";
 import { supabaseServer } from "@/lib/supabase/server";
 import React from "react";
@@ -13,27 +14,7 @@ export default async function page() {
       <div className="max-w-3xl mx-auto md:py-10 h-screen">
         <div className="h-full border rounded-md flex flex-col">
           <ChatHeader user={data?.user} />
-          <div className="flex-1 flex flex-col p-5 h-full overflow-y-auto">
-            <div className="flex-1"></div> {/* chat을 아래에 위치시킴 */}
-            <div className="space-y-7">
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => {
-                return (
-                  <div className="flex gap-2" key={value}>
-                    <div className="h-10 w-10 bg-green-500 rounded-full"></div>
-                    <div className="flex-1">
-                      <div className="flex items-center gap-1">
-                        <h1 className="font-bold">Scott Choo</h1>
-                        <h1 className="text-sm text-gray-400">
-                          {new Date().toDateString()}
-                        </h1>
-                      </div>
-                      <p className="text-gray-300">Hello Guys~</p>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          <ListMessage />
           <ChatInput />
         </div>
       </div>
---
+++ components/ListMessage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ListMessage() {
+  return (
+    <div className="flex-1 flex flex-col p-5 h-full overflow-y-auto">
+      <div className="flex-1"></div> {/* chat을 아래에 위치시킴 */}
+      <div className="space-y-7">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => {
+          return (
+            <div className="flex gap-2" key={value}>
+              <div className="h-10 w-10 bg-green-500 rounded-full"></div>
+              <div className="flex-1">
+                <div className="flex items-center gap-1">
+                  <h1 className="font-bold">Scott Choo</h1>
+                  <h1 className="text-sm text-gray-400">
+                    {new Date().toDateString()}
+                  </h1>
+                </div>
+                <p className="text-gray-300">Hello Guys~</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
