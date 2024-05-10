2ff4b34e0a943f463412fabe61b640949ff55e28
scottXchoo <ckh0601@cau.ac.kr>
2024-05-10 10:38:53 +0900

    chore: update page component to use ChatHeader

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
-import { Button } from "@/components/ui/button";
+import ChatHeader from "@/components/ChatHeader";
+import { supabaseServer } from "@/lib/supabase/server";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const supabase = supabaseServer();
+  const { data } = await supabase.auth.getUser();
+
   return (
     <div className="max-w-3xl mx-auto md:py-10 h-screen">
       <div className="h-full border rounded-md">
-        <div className="h-20">
-          <div className="flex justify-between p-5 border-b">
-            <div>
-              <h1 className="text-xl font-bold">Daily Chat</h1>
-              <div className="flex items-center gap-1">
-                <div className="h-4 w-4 bg-green-400 rounded-full animate-pulse"></div>
-                <h1 className="text-sm text-gray-400">2 onlines</h1>
-              </div>
-            </div>
-            <Button>Login</Button>
-          </div>
-        </div>
+        <ChatHeader user={data?.user} />
       </div>
     </div>
   );
---
+++ components/ChatHeader.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { supabaseBrowser } from "@/lib/supabase/browser";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+
+export default function ChatHeader({ user }: { user: User | null }) {
+  const router = useRouter();
+
+  const handleLoginWithGithub = () => {
+    const supabase = supabaseBrowser();
+    supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: location.origin + "/auth/callback",
+      },
+    });
+  };
+
+  const handleLogout = async () => {
+    const supabase = supabaseBrowser();
+    await supabase.auth.signOut();
+
+    router.refresh();
+  };
+
+  return (
+    <div className="h-20">
+      <div className="flex items-center justify-between p-5 border-b">
+        <div>
+          <h1 className="text-xl font-bold">Daily Chat</h1>
+          <div className="flex items-center gap-1">
+            <div className="h-4 w-4 bg-green-400 rounded-full animate-pulse"></div>
+            <h1 className="text-sm text-gray-400">2 onlines</h1>
+          </div>
+        </div>
+        {user ? (
+          <Button onClick={handleLogout}>Logout</Button>
+        ) : (
+          <Button onClick={handleLoginWithGithub}>Login</Button>
+        )}
+      </div>
+    </div>
+  );
+}
