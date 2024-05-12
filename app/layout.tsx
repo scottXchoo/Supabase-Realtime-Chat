30e2a75a710b7f9d076d6cb87bf3cd7a94088376
scottXchoo <ckh0601@cau.ac.kr>
2024-05-12 14:20:58 +0900

    fix(layout.tsx) : hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import dynamic from "next/dynamic";
 
 const space_Grotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -10,6 +10,10 @@ export const metadata: Metadata = {
   title: "Real-time Chat App",
   description: "Real-time Chat App",
 };
+
+const ThemeProvider = dynamic(() => import("../components/theme-provider"), {
+  ssr: false,
+});
 
 export default function RootLayout({
   children,
