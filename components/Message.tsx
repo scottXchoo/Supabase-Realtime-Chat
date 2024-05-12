a774c4144094fd0e44a041a8f2a263574cf9e0d9
scottXchoo <ckh0601@cau.ac.kr>
2024-05-12 14:44:12 +0900

    ui : MessageMenu component

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,8 +1,20 @@
-import { Imessage } from "@/lib/store/messages";
+import { Imessage, useMessage } from "@/lib/store/messages";
 import Image from "next/image";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Ellipsis } from "lucide-react";
+import { useUser } from "@/lib/store/user";
 
 export default function Message({ message }: { message: Imessage }) {
+  const user = useUser((state) => state.user);
+
   return (
     <div className="flex gap-2">
       <div>
@@ -15,14 +27,49 @@ export default function Message({ message }: { message: Imessage }) {
         />
       </div>
       <div className="flex-1">
-        <div className="flex items-center gap-1">
-          <h1 className="font-bold">{message.users?.display_name}</h1>
-          <h1 className="text-sm text-gray-400">
-            {new Date(message.created_at).toDateString()}
-          </h1>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-1">
+            <h1 className="font-bold">{message.users?.display_name}</h1>
+            <h1 className="text-sm text-gray-400">
+              {new Date(message.created_at).toDateString()}
+            </h1>
+          </div>
+          {message.users?.id === user?.id && <MessageMenu message={message} />}
         </div>
         <p className="text-gray-300">{message.text}</p>
       </div>
     </div>
   );
 }
+
+const MessageMenu = ({ message }: { message: Imessage }) => {
+  const setActionMessage = useMessage((state) => state.setActionMessage);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Ellipsis />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Action</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            document.getElementById("trigger-edit")?.click();
+            setActionMessage(message);
+          }}
+        >
+          Edit
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => {
+            document.getElementById("trigger-delete")?.click();
+            setActionMessage(message);
+          }}
+        >
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
